b755e0303fb493efaea1b00069e575420deb7b9d
John Lam <lamjohn@microsoft.com>
2020-12-07 14:44:16 -0800

    Created Item Model with EnityFramework support

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items;
+
+        ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
